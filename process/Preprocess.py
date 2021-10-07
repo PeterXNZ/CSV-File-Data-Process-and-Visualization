28646b5cbe3744df8defbae80b58d263b8468f50
PeterXNZ <92024442+PeterXNZ@users.noreply.github.com>
2021-10-07 13:05:55 +1030

    Update Preprocess.py

--- process/Preprocess.py
+++ process/Preprocess.py
@@ -13,10 +13,10 @@ for info in os.listdir('''D:\\presen'''):
         for row in csvreader:
             print(row)
             print(f'i is {i}, j is {j}')
-            # 每128个就j加1， 然后就有一个新的文件名
+          
             if i % 1035 == 0:
                 j += 1
-                print(f"csv {j} 生成成功")
+                print(f"csv {j} created")
             # csv_path = os.path.join('../new_csv_file/', 'development (4)/' + str(j) + '.csv')
 
             csv_path = os.path.join(workspace, 'part_{}.csv'.format(j))
@@ -24,8 +24,7 @@ for info in os.listdir('''D:\\presen'''):
 
             # print('/'.join(path.split('/')[:-1]))
             print(csv_path)
-            # 不存在此文件的时候，就创建
-
+          
             if not os.path.exists(os.path.dirname(csv_path)):
                 os.makedirs(os.path.dirname(csv_path))
                 with open(csv_path, 'w', newline='') as info:
@@ -37,7 +36,7 @@ for info in os.listdir('''D:\\presen'''):
                 i += 1
 
 
-            # 存在的时候就往里面添加
+           
             else:
                 with open(csv_path, 'a', newline='') as info:
                     csvwriter = csv.writer(info)
